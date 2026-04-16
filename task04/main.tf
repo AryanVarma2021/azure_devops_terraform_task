09b91a43e09a571d64ae1cce63218fec3e016ffa
aryan_pramodkumarvarma <aryan_pramodkumarvarma@epam.com>
2026-04-16 11:30:17 +0000

    Added some formating in it

--- task04/main.tf
+++ task04/main.tf
@@ -61,11 +61,11 @@ resource "azurerm_network_security_group" "nsg" {
 
 resource "azurerm_network_security_rule" "allow_ssh" {
 
-  name                        = "AllowSSH"
-  priority                    = 1001
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
+  name      = "AllowSSH"
+  priority  = 1001
+  direction = "Inbound"
+  access    = "Allow"
+  protocol  = "Tcp"
 
   source_port_range      = "*"
   destination_port_range = "22"
@@ -82,11 +82,11 @@ resource "azurerm_network_security_rule" "allow_ssh" {
 
 resource "azurerm_network_security_rule" "allow_http" {
 
-  name                        = "AllowHTTP"
-  priority                    = 1002
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
+  name      = "AllowHTTP"
+  priority  = 1002
+  direction = "Inbound"
+  access    = "Allow"
+  protocol  = "Tcp"
 
   source_port_range      = "*"
   destination_port_range = "80"
@@ -126,7 +126,7 @@ resource "azurerm_network_interface" "nic" {
 
 resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
 
-  network_interface_id      = azurerm_network_interface.nic.id
+  network_interface_id = azurerm_network_interface.nic.id
 
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
