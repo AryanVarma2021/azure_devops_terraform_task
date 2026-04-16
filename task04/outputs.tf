ec4a643975985d37eb65cd932996081740277e6d
aryan_pramodkumarvarma <aryan_pramodkumarvarma@epam.com>
2026-04-16 11:45:31 +0000

    Added some formating in it

--- task04/outputs.tf
+++ task04/outputs.tf
@@ -1,11 +1,11 @@
 output "vm_public_ip" {
   description = "VM ip address"
-  value       = azurerm_public_ip.public-ip-address.ip_address
+  value       = azurerm_public_ip.public_ip.ip_address
 
 }
 
 output "vm_fqdn" {
   description = "VM FQDN"
-  value       = azurerm_public_ip.public-ip-address.fqdn
+  value       = azurerm_public_ip.public_ip.fqdn
 
 }
