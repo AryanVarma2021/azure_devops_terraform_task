1cf3f5ee346175df8b7e59e7ac53ed1ecbe52c4f
aryan_pramodkumarvarma <aryan_pramodkumarvarma@epam.com>
2026-04-17 04:04:44 +0000

    Added some formating in it

--- task04/main.tf
+++ task04/main.tf
@@ -172,11 +172,12 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
     publisher = "Canonical"
 
-    offer = "0001-com-ubuntu-server-jammy"
+    offer = "ubuntu-24_04-lts"
 
-    sku = "24_04-lts"
+    sku = "server"
 
     version = "latest"
+
   }
 
 
